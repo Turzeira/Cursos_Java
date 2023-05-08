ef52a316f318e8fa69c65d466f12d3b680856d8f
Arthur Nunes <arthurnunes16@gmail.com>
2023-05-08 15:46:18 -0300

    Removendo Testes

--- tecnicas_de_programacao/src/br/com/unesp/tecnicas_de_programacao/Teste.java
+++
@@ -1,8 +0,0 @@
-package br.com.unesp.tecnicas_de_programacao;
-
-public class Teste {
-
-	public static void main(String[] args) {
-		System.out.println("Testando o meu novo repositorio no GitHub");
-	}
-}
--- tecnicas_de_programacao/src/br/com/unesp/tecnicas_de_programacao/Teste_2.java
+++
@@ -1,5 +0,0 @@
-package br.com.unesp.tecnicas_de_programacao;
-
-public class Teste_2 {
-
-}
